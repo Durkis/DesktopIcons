691efe7b58c0b7dad11bbd0e120a7c65e94f9bd8
Rado <Rado@WIN10BA-R>
2023-08-27 13:34:48 +0200

    Init

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DesktopIcons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            RemoteListView.LVItem[] lvl = RemoteListView.GetListView(   "Progman", "Program Manager", // Program Window Caption and Class
+                                                                        "SysListView32", "FolderView"); // ListView Caption and Class
+            foreach (RemoteListView.LVItem item in lvl)
+                listBox1.Items.Add(item.Name + " @ " + item.Location.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Retrieve the working rectangle from the Screen class
+            // using the PrimaryScreen and the WorkingArea properties.
+            System.Drawing.Rectangle workingRectangle =
+                Screen.PrimaryScreen.WorkingArea;
+            Screen[] myScreens = Screen.AllScreens;
+
+            // Set the size of the form slightly less than size of 
+            // working rectangle.
+            this.Size = new System.Drawing.Size(
+                workingRectangle.Width - 10, workingRectangle.Height - 10);
+
+            // Set the location so the entire form is visible.
+            this.Location = new System.Drawing.Point(5, 5);
+        }
+    }
+}
